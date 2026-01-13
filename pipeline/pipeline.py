4f7ed3e5f049cade7910160e876dabffb4065fe3
Muhammad Aneeb ur Rehman <67867841+aneeb02@users.noreply.github.com>
2026-01-13 14:31:25 +0000

    init venv, used parquet

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -7,11 +7,11 @@ def main():
     
     df = pd.DataFrame({"numbers": [1, 2, 3], "letters": ['a', 'b', 'c']})
     df['month'] = month
-    
-    print(df.head())
+    print(df.head())    
     
     df.to_csv(f"output_month_{month}.csv", index=False)
-    
+
+    #more optimized file format
     df.to_parquet(f"output_month_{month}.parquet", index=False)
     
     print(f"Hello, World! It is the {month} month.")
