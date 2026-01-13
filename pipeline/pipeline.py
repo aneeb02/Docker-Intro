21fd9bd4733fbd9ffaffc569e70ab89db185b9c9
Muhammad Aneeb ur Rehman <67867841+aneeb02@users.noreply.github.com>
2026-01-13 14:23:49 +0000

    building test pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,21 @@
+import sys
+import pandas as pd
+
+def main():
+    print(sys.argv)    
+    month = int(sys.argv[1])
+    
+    df = pd.DataFrame({"numbers": [1, 2, 3], "letters": ['a', 'b', 'c']})
+    df['month'] = month
+    
+    print(df.head())
+    
+    df.to_csv(f"output_month_{month}.csv", index=False)
+    
+    df.to_parquet(f"output_month_{month}.parquet", index=False)
+    
+    print(f"Hello, World! It is the {month} month.")
+    
+    
+if __name__ == "__main__":
+    main()
